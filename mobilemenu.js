d518a6c2be4cf51aac720589521be40eb899a18b
ramneek-ct <156078197+ramneek-ct@users.noreply.github.com>
2024-01-29 21:18:57 +0530

    Add files via upload

---
+++ mobilemenu.js
@@ -0,0 +1,15 @@
+$('.controls').click(function(){
+    $('.line1').toggleClass("line1-transform");
+    $('.line2').toggleClass("line2-transform");
+    $('.line3').toggleClass("line3-transform");
+    $('.toggle-button').toggleClass("toggle-active");
+})
+$('.open').click(function(){
+    $('.toggle-button').hide(1000);
+    $('.open-menu').addClass("open-active");
+});
+
+$('.go-back').click(function(){
+    $('.toggle-button').show();
+    $('.open-menu').removeClass("open-active");
+});
